c663f35b93318489a7dc412fab998dd977b5dcce
Lin Trieu <linnatrieu@gmail.com>
2020-09-09 14:56:01 +0100

    Adds the User Controller class

--- quiz-manager/app/Models/UserPermission.php
+++ quiz-manager/app/Models/UserPermission.php
@@ -1,17 +1,16 @@
 <?php
 
-use  \MyCLabs\Enum\Enum;
+use MyCLabs\Enum\Enum;
 
 /**
  * UserPermission Enum
- * @method static self PERMISSION_LOWEST()
- * @method static self PERMISSION_MIDDLE()
- * @method static self PERMISSION_HIGHEST()
+ * @method static self PERMISSION_RESTRICT()
+ * @method static self PERMISSION_VIEW()
+ * @method static self PERMISSION_EDIT()
  */
 class UserPermission extends Enum
 {
-    public const PERMISSION_RESTRICTED = 1;
+    public const PERMISSION_RESTRICT = 1;
     public const PERMISSION_VIEW = 2;
     public const PERMISSION_EDIT = 3;
-
 }
