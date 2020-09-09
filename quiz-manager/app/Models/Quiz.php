4ca76fa2bb9941aabe5213a098dc708993fde40c
Lin Trieu <linnatrieu@gmail.com>
2020-09-09 16:51:33 +0100

    Adds Quiz model

--- quiz-manager/app/Models/Quiz.php
+++ quiz-manager/app/Models/Quiz.php
@@ -4,7 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Quiz
+ *
+ * @property  int $id
+ * @property string $title
+ * @property string $icon
+ */
 class Quiz extends Model
 {
-    //
+    protected $table = 'quiz';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'icon'
+    ];
 }
