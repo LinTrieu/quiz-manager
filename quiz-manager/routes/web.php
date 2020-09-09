c621d53479bd1fb95c93d10913a4b1603af640de
Lin Trieu <linnatrieu@gmail.com>
2020-09-09 16:07:58 +0100

    Set Auth registration routes to false

--- quiz-manager/routes/web.php
+++ quiz-manager/routes/web.php
@@ -18,9 +18,9 @@ Route::get('/', function () {
 });
 
 Auth::routes([
-    'register' => true, // Registration Routes
-    'reset' => true, // Password Reset Routes
-    'verify' => true, // Email Verification Routes
+    'register' => false, // Registration Routes
+    'reset' => false, // Password Reset Routes
+    'verify' => false, // Email Verification Routes
 ]);
 
 Route::get('/home', 'HomeController@index')->name('home');
