9becaa862f152ec375cb5bc3633326e0746f3587
Lin Trieu <linnatrieu@gmail.com>
2020-09-10 16:28:28 +0100

    Adds feature tests for QuizTest

---
+++ quiz-manager/tests/Feature/QuizTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Question;
+use App\Models\Quiz;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use Mockery;
+
+class QuizTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testUserCanViewQuizList(): void
+    {
+        $this->loginWithFakeUser();
+        $response = $this->get('/quiz/1');
+
+        $response->assertSuccessful();
+        $response->assertViewIs('quiz.quiz');
+    }
+
+    public function testUserOnlySeesQuestionsBelongingToAQuiz(): void
+    {
+        $this->loginWithFakeUser();
+        $quizOne = factory(Quiz::class)->create([
+            'id' => 1,
+            'title' => 'Geography Quiz',
+        ]);
+
+        $quizTwo = factory(Quiz::class)->create([
+            'id' => 2,
+            'title' => 'Maths Quiz',
+        ]);
+
+        $questionQuizOne = factory(Question::class)->create([
+            'quiz_id' => 1,
+        ]);
+        $questionQuizTwo = factory(Question::class)->create([
+            'quiz_id' => 2,
+        ]);
+
+        $response = $this->get('/quiz/1');
+        $response->assertSuccessful();
+
+        $response->assertSee($questionQuizOne->description);
+        $response->assertDontSee($questionQuizTwo->description);
+    }
+
+}
