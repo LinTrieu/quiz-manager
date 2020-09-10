7096aadbe471ebda1fc063ceab41b9b2c0487dc1
Lin Trieu <linnatrieu@gmail.com>
2020-09-10 17:04:10 +0100

    Adds button for Answers

--- quiz-manager/resources/views/quiz/quiz.blade.php
+++ quiz-manager/resources/views/quiz/quiz.blade.php
@@ -3,20 +3,23 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-9">
                 <div class="card">
                     <div class="card-header"> {{ __('Quiz') }} </div>
 
                     <div class="card-body">
                         @foreach ($questions as $question)
-                            <p> Question {{ $question['id'] }}. {{ $question['description'] }} </p>
-                            <p> Options </p>
-                            <li> A: {{ $question['option_a'] }} </li>
-                            <li> B: {{ $question['option_b'] }} </li>
-                            <li> C: {{ $question['option_c'] }} </li>
-                            <li> D: {{ $question['option_d'] }} </li>
-                            <li> E: {{ $question['option_e'] }} </li>
-                            <br>
+                            <div class="mb-5">
+                                <p> Question {{ $question['id'] }}. {{ $question['description'] }} </p>
+                                <p> Options </p>
+                                <li> A: {{ $question['option_a'] }} </li>
+                                <li> B: {{ $question['option_b'] }} </li>
+                                <li> C: {{ $question['option_c'] }} </li>
+                                <li> D: {{ $question['option_d'] }} </li>
+                                <li> E: {{ $question['option_e'] }} </li>
+                                <br>
+                                <a href="#" class="card-link">Show Answer</a>
+                            </div>
                         @endforeach
                     </div>
                     <div class="card-footer">
