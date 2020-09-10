342adc1666077747e2c2d3dc0f6c020bfa1fc624
Lin Trieu <linnatrieu@gmail.com>
2020-09-10 11:53:25 +0100

    Adds Quiz List tests

--- quiz-manager/tests/Feature/QuizListTest.php
+++ quiz-manager/tests/Feature/QuizListTest.php
@@ -9,15 +9,26 @@ class QuizListTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testUserCanViewAListOfAllQuizzes(): void
+    public function testUserCanViewQuizList(): void
     {
         $this->loginWithFakeUser();
+        $response = $this->get('/quiz');
 
+        $response->assertSuccessful();
+        $response->assertViewIs('quiz.quiz_list');
     }
 
     public function testUserCannotViewQuizzesWhenGuest(): void
     {
+        $response = $this->get('/quiz');
+
+        $response->assertRedirect('/login');
+        $this->assertGuest();
     }
 
+    public function testAllQuizzesAreListedOnQuizList(): void
+    {}
 
+    public function testEachQuizInListHasALink(): void
+    {}
 }
