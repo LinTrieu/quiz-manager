d9f7affc58b6566a673fbaa70161c709468bd549
LinTrieu <linnatrieu@gmail.com>
2022-02-04 20:36:19 -0800

    upgrade quiz and question factory

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -1,32 +1,28 @@
 <?php
 
-/** @var Factory $factory */
+namespace Database\Factories;
 
-use App\Models\Question;
-use App\Models\Quiz;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Faker\Generator as Faker;
-use Illuminate\Database\Eloquent\Factory;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
-
-$factory->define(Question::class, function (Faker $faker) {
-    return [
-        'description' => $this->faker->sentence . '?',
-        'quiz_id' => Quiz::all()->random()->id,
-        'answer_key' => $this->faker->randomElement(['A','B','C','D','E']),
-        'option_a' => $this->faker->word,
-        'option_b' => $this->faker->word,
-        'option_c' => $this->faker->word,
-        'option_d' => $this->faker->word,
-        'option_e' => $this->faker->word,
-    ];
-});
+class QuestionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'description' => $this->faker->sentence . '?',
+            'quiz_id' => Quiz::all()->random()->id,
+            'answer_key' => $this->faker->randomElement(['A','B','C','D','E']),
+            'option_a' => $this->faker->word,
+            'option_b' => $this->faker->word,
+            'option_c' => $this->faker->word,
+            'option_d' => $this->faker->word,
+            'option_e' => $this->faker->word,
+        ];
+    }
+}
--- database/factories/QuizFactory.php
+++ database/factories/QuizFactory.php
@@ -1,24 +1,21 @@
 <?php
 
-/** @var Factory $factory */
+namespace Database\Factories;
 
-use App\Models\Quiz;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Faker\Generator as Faker;
-use Illuminate\Database\Eloquent\Factory;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
-
-$factory->define(Quiz::class, function (Faker $faker) {
-    return [
-        'title' => $this->faker->word . ' ' . $this->faker->word . ' ' . $this->faker->word,
-    ];
-});
+class QuizFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->word . ' ' . $this->faker->word . ' ' . $this->faker->word,
+        ];
+    }
+}
