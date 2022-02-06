8f1988427d4f7acf8222ecd2abc83f8763b33ae4
LinTrieu <linnatrieu@gmail.com>
2022-02-05 22:51:37 -0800

    fix question factory foreign key seeding

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use Database\Factories\UserFactory;
+use App\Database\Factories\UserFactory;
 
 class UserSeeder extends Seeder
 {
