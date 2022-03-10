af2bed4876a209f4e0ddd01ae050b27e2dff5ca9
LinTrieu <linnatrieu@gmail.com>
2022-03-09 21:07:37 -0800

    remove main logout button in navbar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -88,17 +88,6 @@
                                 </form>
                               </div>
                         </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('logout') }}"
-                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </li>
                         @endguest
                     </ul>
                 </div>
